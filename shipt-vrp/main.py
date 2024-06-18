9d4d697a10da3c6798938adf96a2db93eeda0da9
alexjmeyer92 <alex.j.meyer15@gmail.com>
2024-06-18 15:53:54 -0500

    refactor functions to utils

--- shipt-vrp/main.py
+++ shipt-vrp/main.py
@@ -3,93 +3,10 @@ Basic CVRP implementation representing routing strategy used at Shipt
 """
 
 # imports
-from numpy import zeros, sqrt
-from numpy.random import randint, seed
-
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
 
-
-def input_data(n_vehicles=5, max_capacity=10):
-    """
-    Purpose:
-    """
-
-    # end def
-    # randomly generate nodes x,y coords
-    seed(1234)  # set seed to replicate matrix
-    grid_min, grid_max = (
-        0,
-        100,
-    )  # set x,y coordinate space (represents minutes of travel time)
-    nodes = [randint(low=grid_min, high=grid_max, size=2) for x in range(14)]
-
-    # nodes[1], nodes[2], nodes[3] represent stores w/ parcels to pick up. Remaining nodes are
-    # customer delivery locations
-    del_reqs = [
-        [1, 4],
-        [1, 5],
-        [1, 6],
-        [2, 7],
-        [2, 8],
-        [2, 9],
-        [2, 10],
-        [3, 11],
-        [3, 12],
-        [3, 13],
-    ]
-    demands = [0, 3, 4, 3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
-
-    # build distance matrix
-    n_nodes = len(nodes)
-
-    dist_matrix = zeros((n_nodes, n_nodes), dtype=int)
-
-    # Calc distances & add to matrix
-    # Euclidean distance for simplicity (well pretend is estimated minutes)
-    for i in range(
-        1, n_nodes
-    ):  # skip 0 index because all "depot" distances are 0 as routes may start any where
-        for j in range(i + 1, n_nodes):
-            dist = sqrt(
-                (nodes[j][0] - nodes[i][0]) ** 2 + ((nodes[j][1] - nodes[i][1]) ** 2)
-            )
-            rounded_distance = int(round(dist))
-            dist_matrix[i][j] = rounded_distance
-            dist_matrix[j][i] = rounded_distance
-    inputs = {
-        "nodes": nodes,
-        "distance_matrix": dist_matrix,
-        "pickups_deliveries": del_reqs,
-        "demands": demands,
-        "num_vehicles": n_vehicles,
-        "vehicle_capacities": [max_capacity] * n_vehicles,
-        "depot": 0,  # has no effect on solution bc depot distances are all filled as 0
-    }
-    return inputs
-
-
-# print solution
-def print_solution(data, manager, routing, solution):
-    """Prints solution on console."""
-    print(f"Objective: {solution.ObjectiveValue()}")
-    total_distance = 0
-    for vehicle_id in range(data["num_vehicles"]):
-        index = routing.Start(vehicle_id)
-        plan_output = f"Route for vehicle {vehicle_id}:\n"
-        route_distance = 0
-        while not routing.IsEnd(index):
-            plan_output += f" {manager.IndexToNode(index)} -> "
-            previous_index = index
-            index = solution.Value(routing.NextVar(index))
-            route_distance += routing.GetArcCostForVehicle(
-                previous_index, index, vehicle_id
-            )
-        plan_output += f"{manager.IndexToNode(index)}\n"
-        plan_output += f"Distance of the route: {route_distance}m\n"
-        print(plan_output)
-        total_distance += route_distance
-    print(f"Total Distance of all routes: {total_distance}m")
+from utils import input_data, estimate_counterfactual_cost, parse_solution
 
 
 def run_problem():
@@ -168,36 +85,32 @@ def run_problem():
 
     # Solve the problem.
     solution = routing.SolveWithParameters(search_parameters)
-    print(solution)
+
     # Print solution on console.
     if solution:
-        print_solution(data, manager, routing, solution)
+        output = parse_solution(data, manager, routing, solution)
+        performance_eval = estimate_counterfactual_cost(data, output)
+        return {"route_output": output, "eval": performance_eval}
 
 
 if __name__ == "__main__":
-    run_problem()
-    # dat = input_data()
-    # print(dat)
-    # print(len(dat["demands"]) == len(dat["nodes"]))
-    # print(len(dat["distance_matrix"]) == len(dat["nodes"]))
-    # print(len(dat["distance_matrix"][0]) == len(dat["nodes"]))
-    # print(sum(dat["demands"]))
-    # print(len(dat["pickups_deliveries"]))
-
-    # print(dat["distance_matrix"])
-    # print(dat["nodes"])
+    output = run_problem()
+    print(output)
+    dat = input_data()
+    print(dat)
 
     # # shitty plot of nodes (need to figureout how to add routes)
     # import matplotlib.pyplot as plt
+    # from plot_utils import node_attrs
 
-    # coords = dat["nodes"]
+    # nodes =
 
-    # x, y = zip(*coords)
+    # x, y = zip(*nodes)
 
     # plt.scatter(x, y, color="green", marker="o")
 
-    # for i, (x, y) in enumerate(coords):
-    #     plt.text(x, y, f"{i} || {x},{y}", fontsize=9, ha="right")
+    # for i, (x, y) in enumerate(nodes):
+    #     plt.text(x, y, f"{i} || {x},{y}", fontsize=9, ha="left")
 
     # plt.xlabel("x")
     # plt.ylabel("y")
